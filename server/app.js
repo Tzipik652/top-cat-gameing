1d8e76e57ff4a3e60608cf508e8b99b7ed85556d
Tzipik652 <tzipik652@gmail.com>
2025-10-16 19:48:58 +0300

    feat: initialize Express server with user routes

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const userRoutes = require("./routes/users")
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
