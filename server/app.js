5985d3f8324c1b8596544b0c18903515740eabb3
Tzipik652 <tzipik652@gmail.com>
2025-10-19 11:18:21 +0300

    feat: add CORS support to the server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoutes = require("./routes/users")
-
+app.use(cors()); 
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
